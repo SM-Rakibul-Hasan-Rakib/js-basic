6d51153d4234cc985067c83d02e72c2b59affd7a
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-01-29 21:51:05 +0600

    kj

---
+++ look-task.js
@@ -0,0 +1,14 @@
+// let day = 60;
+// let message = 'I will invite';
+
+// while(day >0) {
+//   console.log(message);
+//   day--;
+
+let day = 180;
+let message = 'Hard working for 6 month';
+
+while(day > 0){
+  console.log(day);
+  day--;
+}
