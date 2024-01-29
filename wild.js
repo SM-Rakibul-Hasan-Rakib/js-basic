74aa4c8921ad3e958a68704bda1cc9b88afcdc4d
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-01-29 21:32:58 +0600

    j

---
+++ wild.js
@@ -0,0 +1,22 @@
+// const number = 10;
+// if(number < 7){
+//   console.log('choto akta number paise');
+
+// }
+// console.log('something is wrong');
+
+// *****************while-loop*********
+// const num = 0;
+// while(num < 5){
+//   console.log('loooooooooping');
+// // }
+// let num = 1;
+// while (num <= 10){
+//   console.log(num);
+//   num++;
+// }
+let num = 50;
+while(num <= 100){
+  console.log(num);
+  num++;
+}
