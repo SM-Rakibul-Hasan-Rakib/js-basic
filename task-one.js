f74afaf54c2f2a87cde83d7ea7a491b99b365f9e
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-01-29 17:37:34 +0600

    pushes

--- task-one.js
+++ task-one.js
@@ -1,18 +1,28 @@
-// array with 5 elements containing
-var fruits = ["apple", "orange", "banana", "grape", "kiwi"];
-// log 3rd index element
-console.log(fruits[3]);
+// // array with 5 elements containing
+// var fruits = ["apple", "orange", "banana", "grape", "kiwi"];
+// // log 3rd index element
+// console.log(fruits[3]);
 
-//change value with 2nd index
-fruits[2] = "jambura";
-fruits[4] = "tor nana";
-// final result
-console.log(fruits);
+// //change value with 2nd index
+// fruits[2] = "jambura";
+// fruits[4] = "tor nana";
+// // final result
+// console.log(fruits);
 
 //.......................................
 
-//array with 5 elements ccontaining fruits
-var fru = ["rafi", "rakib", "sohel", "sifat", "tamim", "emon",];
+// //array with 5 elements ccontaining fruits
+// var fru = ["rafi", "rakib", "sohel", "sifat", "tamim", "emon",];
 
-//4,5 value changes
-console.log()
+// //4,5 value changes
+// console.log(fru[4]);
+// // value changes
+// fru [1] = "todr sobar abba ami";
+
+// console.log(fru);
+// array in 
+var name = ["rafi", "rakib", "sifat", "sohel", "emon", "tamim", "saghor", "shafin"]
+
+name[4] = "faw khaila khaw na khaila bhari jaw";
+ 
+console.log(name);
