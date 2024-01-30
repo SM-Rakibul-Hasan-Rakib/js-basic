152dda6a2cd9b755b8defcd49c8326df1ca0cc83
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-01-30 16:23:17 +0600

    j

---
+++ evan.js
@@ -0,0 +1,6 @@
+let number = 78;
+for(let number = 78; number <= 100; number++){
+  if(number % 2 ===0 ){
+    console.log(number);
+  }
+}
---
+++ odd.js
@@ -0,0 +1,15 @@
+//odd number in 61-100
+// let number = 61;
+// for(let number = 61; number <= 100; number++){
+//   if(number % 2 !==0){
+//     console.log(number)
+//   }
+// }
+
+// even number in 61-100
+let number = 61;
+for(let number = 1;number <=100; number++ ){
+  if(number % 2 === 0){
+    console.log(number);
+  }
+}
