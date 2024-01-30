7abddbb598fac53ce274470ed56562ad1f21fe0b
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-01-30 14:33:13 +0600

    k

---
+++ for-sum.js
@@ -0,0 +1,7 @@
+let sum = 0;
+
+for(let i = 0; i<=20; i++){
+  sum =  sum + i;
+  console.log(i);
+}
+console.log('some of number from 11 to 20 is',sum)
---
+++ for.js
@@ -0,0 +1,6 @@
+// for(let a = 1; a <= 100; a++){
+//   console.log(a);
+// }
+for (let a = 0; a <= 20; a= a-1){
+  console.log(a);
+}
