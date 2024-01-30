df275582076eadf1acb13eb2cbaacbaaf11dbb75
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-01-30 15:42:14 +0600

    f

--- p.js
+++ p.js
@@ -8,10 +8,10 @@
 //   count++;
 // }
 //while message
-const message = "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
-let count = 1;
+const message = "I will invest at least 6 hrs every single day for next 60 days!"
+let count = 0;
 
-while(count < 50){
-  console.log(count);
+while(count < 60){
+  console.log(message);
   count++;
 }
