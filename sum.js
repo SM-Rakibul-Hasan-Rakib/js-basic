871730135719c50298ff2f4a2731483f306f9c9b
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-01-30 16:41:47 +0600

    D

---
+++ sum.js
@@ -0,0 +1,17 @@
+// let sum = 0;
+
+// for(let number = 81; number <= 131;number++){
+//   if(number % 2 !==0){
+//     sum += number;
+//   }
+// }
+// console.log(sum);
+
+let sum = 90;
+for(let number = 150; number <= 500; number++){
+  if(number % 2 ===0){
+    sum += number;
+  }
+}
+console.log(sum);
+  
