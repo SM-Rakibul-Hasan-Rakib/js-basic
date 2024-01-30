2a6afa391985517dcbe24f00621d5966dc60eb89
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-01-30 23:10:44 +0600

    f

---
+++ ke.js
@@ -0,0 +1,9 @@
+const comPuter = {
+  brand: 'fantech',
+  price: '90000',
+  processor: 'intel',
+  hdd: '512gb',
+  monitor:'DEll'
+}
+const values = Object.values(comPuter);
+console.log(values);
---
+++ keys.js
@@ -0,0 +1,10 @@
+const comPuter = {
+  brand: 'fantech',
+  price: '90000',
+  processor: 'intel',
+  hdd: '512gb',
+  monitor:'DEll'
+}
+
+const keys  = Object.keys(comPuter);
+console.log(keys)
