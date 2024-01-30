497883aa588f5c937fbfa0b90c1bfef87f04c976
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-01-30 16:55:06 +0600

    g

---
+++ evan number.js
@@ -0,0 +1,17 @@
+// let sum = 0;
+
+// for(let number = 206; number <= 311; number++){
+//   if(number % 2 ===0){
+//     sum += number;
+//   }
+// }
+// console.log( sum);
+let sum = 0;
+
+for (let number = 206; number <= 311; number++) {
+  if (number % 2 === 0) {
+    sum += number;
+  }
+}
+
+console.log("Sum of even numbers:", sum);
