7da66236f92df74f8e798f7e943c8cb10433f8b3
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-01-30 21:36:11 +0600

    gh

---
+++ abc.js
@@ -0,0 +1,18 @@
+const address = 'andorkilla';
+const part = address.slice(2, 5);
+
+console.log(part);
+
+const sentence = 'I am a good student and hard working person';
+console.log(sentence.split('a'));
+
+const friendStar = 'Rahim,kahim,tamim,badam,chadam,khailm';
+ const friends = friendStar.slice(', ');
+ console.log(friendStar);
+
+
+//  ************
+ const Star = ['Rahim,kahim,tamim,badam,chadam,khailm'];
+
+ console.log(Star.join());
+ console.log(Star.join('|'));
