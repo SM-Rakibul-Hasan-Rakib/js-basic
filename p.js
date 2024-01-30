beadb5420e26dacac4b87f30ea55915dca085015
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-01-30 15:32:19 +0600

    f

---
+++ p.js
@@ -0,0 +1,17 @@
+// // while message
+// const messages = ['I  will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.'];
+// let count = 0;
+
+// //result
+// while(count < 60) {
+//   console.log(messages);
+//   count++;
+// }
+//while message
+const message = "I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+let count = 1;
+
+while(count < 50){
+  console.log(count);
+  count++;
+}
