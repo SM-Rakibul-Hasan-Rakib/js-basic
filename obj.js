8135edba87061d00d3ae8d4fb25856c24503a86e
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-01-30 22:42:39 +0600

    obj value

---
+++ obj.js
@@ -0,0 +1,21 @@
+const person = {
+  name: 'Rakib Ahmed',
+  age: 18,
+  profession: 'developer',
+  salery: 30000,
+  married: false,
+  'fav places' : ['bandorban', 'saintmartine', 'kukata']
+}
+
+person.salery = 40000;
+person.age = 500;
+person['fav places']= ['jamu na'];
+person.married = true;
+console.log(person);
+//console.log(person);
+//console.log(person.profession)
+// const inccome = person.salery;
+//console.log(inccome);
+
+//bracket Notation
+//
