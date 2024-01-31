9acd1fcc99b2f2d83f9c527555e4786dd573c084
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-01-31 23:04:45 +0600

    hfhjh

---
+++ onj.js
@@ -0,0 +1,13 @@
+// my cat
+const myPet = {
+ name : "Travis",
+ age : 2,
+ food :"fish",
+ isMale : true,
+ color : "white",
+ specis : "Hullo biral"
+};
+for(let key in myPet) {
+  let value = myPet[key];
+  console.log(value);
+}
