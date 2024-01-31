f5e52099ceb1805e77933bb1dfa16fc42d72598d
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-01-31 21:58:31 +0600

    hi

---
+++ st.js
@@ -0,0 +1,13 @@
+// const x = "Hello world";
+// const y = "hello";
+
+// console.log(x.toLocaleLowerCase().includes(y.toUpperCase));
+
+const x = "hello";
+const y = "hello   ";
+
+if(x ===y.trim()) {
+  console.log("both are same");
+}else{
+  console.log("both are not same");
+}
