c254a1eac279cc006ee311601b0e2a71103790ca
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-01-31 21:05:05 +0600

    jani na

--- gf.js
+++ gf.js
@@ -1,5 +1,15 @@
 const nums = [23, 43, 2, 5, 346, 203, 2]
 
+sumOfOdd = 0;
+sumOfEven = 0;
+
 for(let i = 0; i <nums.length; i++){
-  console.log(nums[i]);
+  const x = nums[i];
+
+  if(x % 2 ===1){
+    sumOfOdd += x;
+  }else {
+    sumOfEvev += x;
+  }
 }
+console.log(sumOfEven);
