e867eda53a94aee148e3612b9eb778da31f5434c
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-02-01 15:14:40 +0600

    m

---
+++ ok.js
@@ -0,0 +1,11 @@
+const num = [1, 2, 3, 4, 5, 6, 7, 8,];
+// const rev = num.reverse();
+// console.log(rev);
+
+const rev_num = [];
+
+for (const num of numbers) {
+  rev_num.unshift(num);
+}
+// console.log(rev_num);
+const reversed_numbers = []
---
+++ reb.js
@@ -0,0 +1,4 @@
+const names =['elon, ', 'mask', 'bill', 'waren'];
+
+const arr1 = names.reverse(names);
+console.log(arr1);
